b8a5dcbdf0af4d7a6f7f4b889fd246840c792fc3
Lorenzo Zanotto <info@lorenzozanotto.com>
2022-05-06 16:00:51 +0200

    chore: fix desktop margin from top area

--- components/Desktop.tsx
+++ components/Desktop.tsx
@@ -1,6 +1,6 @@
 function Desktop() {
     return ( 
-        <div className="w-full flex-1 mt-[6px] rounded-b-[12px] bg-desktop-background">
+        <div className="w-full flex-1 mt-[2px] rounded-b-[12px] bg-desktop-background">
             <h1>Title cool</h1>
         </div>
      );
