a8a8efe0547ed3a012f00f8ffc2cf27d32ee01ad
Lorenzo Zanotto <lorenzo@LorenzoookA2485.lan>
2022-10-10 17:55:50 +0200

    add tmp page

---
+++ pages/disney.tsx
@@ -0,0 +1,7 @@
+function DisneyPage() {
+    return ( 
+        <h1 className="text-[94px] text-blue-700 underline"><a href="/dark/ds.pdf">Cliccami</a></h1>
+     );
+}
+
+export default DisneyPage;
