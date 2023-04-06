2e37c20a3f043758c0a0cbfe9cbe87252b6b27ae
Lorenzo Zanotto <info@lorenzozanotto.com>
2023-04-06 14:36:08 +0200

    redirect to bento.me

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://bento.me/zanotto',
+        permanent: false
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
